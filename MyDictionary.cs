07173fb4bd0601788ccefbb3a267a848cbbe0aba
Yagmur <akin26321@gmail.com>
2021-01-23 20:45:18 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdevDictionary
+{
+    class MyDictionary <T1, T2>
+    {
+        {
+          T1[] keys;
+          T2[] values;
+
+        public  MyDictionary()
+        {
+            keys = new  T1[0];
+            values = new T2[0];
+        }
+
+        public void Add(T1 Key, T2 Value)
+        {
+            T1[] tempArrayKeys = keys;
+            T2[] tempArrayValues = Value;
+
+            keys = new  T1[keys.Length + 1];
+            values = new T2[keys.Length + 1];
+
+            for (int i = 0; i < tempArrayKeys.Length; i++)
+            {
+                keys[i] = tempArrayKeys[i];
+                values[i] = tempArrayValues[i];
+            }
+
+            keys[keys.Length - 1] = Key;
+            keys[keys.Length - 1] = Value;
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OdevDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,int > myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add(" ALİ ",3);
+            myDictionary.Add(" Mehmet ",4);
+            myDictionary.Add(" Ahmet ",5);
+
+
+        }
+    }
+}
